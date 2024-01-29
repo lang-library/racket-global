1c9a0d5e50b0926069e1f91f077f01bf6ad7cccc
JavaCommons Technologies <javacommons@gmail.com>
2024-01-30 04:36:48 +0900

    .

--- main.rkt
+++ main.rkt
@@ -4,13 +4,29 @@
 (require ffi/unsafe
          ffi/unsafe/define)
 (require json)
+(require compatibility/defmacro)
 
 (define :null ':null)
 
 (define (to-json %x) (jsexpr->string %x #:null :null))
+
 (define (from-json %json) (string->jsexpr %json #:null :null))
+
+(define-macro (define-json-api . $args)
+  ;(print $args)
+  `(begin
+    (define-ffi-definer
+      ,(string->symbol (string-append "define-" (list-ref $args 0)))
+      (ffi-lib ,(list-ref $args 1)))
+    (,(string->symbol (string-append "define-" (list-ref $args 0)))
+     Call (_fun _string _string -> _string))
+    (define (,(string->symbol (string-append "call-" (list-ref $args 0))) name args)
+      (from-json (Call name (to-json args))))
+    )
+  )
 
 (provide
  to-json
  from-json
+ define-json-api
  )
