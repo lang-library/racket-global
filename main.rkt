ba22194a203687d7e7f848e9c0cba85c0a00f397
JavaCommons Technologies <javacommons@gmail.com>
2024-01-30 12:14:30 +0900

    .

--- main.rkt
+++ main.rkt
@@ -4,16 +4,16 @@
 (require json)
 (require compatibility/defmacro)
 
-(define :null ':null)
+;;(define :null ':null)
 
-(define (to-json %x) (jsexpr->string %x #:null :null))
+(define (to-json %x) (jsexpr->string %x #:null ':null))
 
-(define (from-json %json) (string->jsexpr %json #:null :null))
+(define (from-json %json) (string->jsexpr %json #:null ':null))
 
 (define (json-api-safe-car %x)
   (cond
     ((list? %x)
-     (if (zero? (length %x)) :null (car %x)))
+     (if (zero? (length %x)) ':null (car %x)))
     (#t %x)))
 
 (define-macro (define-json-api . $args)
